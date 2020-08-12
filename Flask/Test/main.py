d046224f1651a3789054f37d633eb3d85f02cde7
Marc Partensky <marc.partensky@gmail.com>
2020-08-13 01:38:10 +0200

    documenting for documenting

--- Flask/Test/main.py
+++ Flask/Test/main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
+"""Testing flask cause its flask."""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
+    """Main function."""
     print('salut')
     return 'wesh'
 
